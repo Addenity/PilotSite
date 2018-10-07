a5387749d3768baff8052f48c1e4df94c1d9d403
Yasser Soliman <yassersoliman_1@hotmail.com>
2018-10-07 01:04:53 -0400

    Pilot site

---
+++ script.js
@@ -0,0 +1,9 @@
+// Write JavaScript here 
+ function toggleGallery(){
+   var img = document.getElementById("MenuGallery");
+     if(img.style.display === "none"){
+         img.style.display = "block";
+           } else {
+               img.style.display = "none";
+                 }
+                 }
